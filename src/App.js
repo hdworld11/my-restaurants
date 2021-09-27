f46d57f871dea2c40a8608c02d25fa6e7c274cc9
Harshita Daddala <daddala@amazon.com>
2021-09-27 09:13:16 -0400

    feat: Initial commit of project structure, license, and readme.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { createMap} from "maplibre-gl-js-amplify";
+import { drawPoints } from "maplibre-gl-js-amplify";
+import "maplibre-gl/dist/maplibre-gl.css";
+Amplify.configure(awsconfig);
+
+async function initializeMap() {
+  const map = await createMap({
+      container: "map", // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js-docs/api/map/
+      center: [-122.483696, 37.833818],
+      zoom: 11,
+  })
+
+  map.on("load", function () {
+    drawPoints("mySourceName", // Arbitrary source name
+        [
+            {
+              coordinates: [-122.483696, 37.833818],
+              title: "Golden Gate Bridge",
+              address: "A suspension bridge spanning the Golden Gate",
+            },
+            {
+              coordinates: [- 122.4770, 37.8105],
+            },
+        ], // An array of coordinate data, an array of Feature data, or an array of [NamedLocations](https://github.com/aws-amplify/maplibre-gl-js-amplify/blob/main/src/types.ts#L8)
+        map,
+        {
+            showCluster: true,
+            unclusteredOptions: {
+                showMarkerPopup: true,
+            },
+            clusterOptions: {
+                showCount: true,
+            },
+        }
+    );
+});
+}
+
+initializeMap();
 
 function App() {
   return (
