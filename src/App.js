b5b1e0dcae350e98625caeb13290218e16021deb
Harshita Daddala <daddala@amazon.com>
2021-09-27 13:17:53 -0400

    make css changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ Amplify.configure(awsconfig);
 async function initializeMap() {
   const map = await createMap({
       container: "map", // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js-docs/api/map/
-      center: [-122.483696, 37.833818],
+      center: [-73.98597609730648, 40.751874635721734],
       zoom: 11,
   })
 
@@ -18,12 +18,19 @@ async function initializeMap() {
     drawPoints("mySourceName", // Arbitrary source name
         [
             {
-              coordinates: [-122.483696, 37.833818],
-              title: "Golden Gate Bridge",
-              address: "A suspension bridge spanning the Golden Gate",
+              coordinates: [-73.98709247500821, 40.718839863699905],
+              title: "My Restaurant - Lower East Side",
+              address: "102 Norfolk St, New York, NY 10002",
             },
             {
-              coordinates: [- 122.4770, 37.8105],
+              coordinates: [-73.9893305444102, 40.76329636720047],
+              title: "My Restaurant - Hell's Kitchen",
+              address: "725 9th Ave, New York, NY 10019",
+            },
+            {
+              coordinates: [-73.95621342276895, 40.77225519589616],
+              title: "My Restaurant - Upper East Side",
+              address: "300 E 77th St, New York, NY 10075",
             },
         ], // An array of coordinate data, an array of Feature data, or an array of [NamedLocations](https://github.com/aws-amplify/maplibre-gl-js-amplify/blob/main/src/types.ts#L8)
         map,
@@ -45,20 +52,8 @@ initializeMap();
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Restaurant</h1>
+      <div id="map"></div>
     </div>
   );
 }
